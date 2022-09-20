5ada6c19bf625c7550958fc01f36815a8e30573c
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-09-20 09:55:29 +0200

    Import additional structures to future settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExampleModule } from './example/example.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [ExampleModule],
+  imports: [ExampleModule, AuthModule, UserModule, TodoModule],
   controllers: [],
   providers: [],
 })
